49f5faa463ed813fc9e302716d435e43347a4018
Abhishek Bansal <doscover.ab@gmail.com>
2019-06-13 19:10:43 +0530

    initial commit

---
+++ app/src/main/java/com/example/moshitest/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.example.moshitest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.annotation.RawRes
+import android.util.Log
+import com.squareup.moshi.*
+import com.squareup.moshi.adapters.PolymorphicJsonAdapterFactory
+import java.lang.reflect.Type
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val moshi = Moshi.Builder()
+            // ... add your own JsonAdapters and factories ...
+            .add(SkipBadElementsListAdapter.Factory)
+            .add(
+                PolymorphicJsonAdapterFactory.of(Vehicle::class.java, "type")
+                    .withSubtype(Car::class.java, "car")
+                    .withSubtype(Truck::class.java, "truck")
+            )
+            .add(
+                PolymorphicJsonAdapterFactory.of(Tyre::class.java, "type")
+                    .withSubtype(MRFTyre::class.java, "mrf")
+                    .withSubtype(ApolloTyre::class.java, "apollo")
+            )
+            .build()
+
+        val string = getFileContent(R.raw.test_object)
+        val adapter: JsonAdapter<List<Vehicle>> =
+            moshi.adapter(Types.newParameterizedType(List::class.java, Vehicle::class.java))
+
+        string?.let {
+            val vehicles: List<Vehicle>? = adapter.fromJson(string)
+            Log.d("test", "success")
+        }
+    }
+
+    private fun getFileContent(@RawRes id: Int): String? {
+        return try {
+            val inputStream = resources.openRawResource(id)
+            val b = ByteArray(inputStream.available())
+            inputStream.read(b)
+            String(b)
+        } catch (e: Exception) {
+            // e.printStackTrace();
+            null
+        }
+    }
+
+    class SkipBadElementsListAdapter(private val elementAdapter: JsonAdapter<Any?>) :
+        JsonAdapter<List<Any?>>() {
+        object Factory : JsonAdapter.Factory {
+            override fun create(type: Type, annotations: Set<Annotation>, moshi: Moshi): JsonAdapter<*>? {
+                if (annotations.isNotEmpty() || Types.getRawType(type) != List::class.java) {
+                    return null
+                }
+                val elementType = Types.collectionElementType(type, List::class.java)
+                val elementAdapter = moshi.adapter<Any?>(elementType)
+                return SkipBadElementsListAdapter(elementAdapter)
+            }
+        }
+
+        override fun fromJson(reader: JsonReader): List<Any?>? {
+            val result = mutableListOf<Any?>()
+            reader.beginArray()
+            while (reader.hasNext()) {
+                try {
+                    val peeked = reader.peekJson()
+                    result += elementAdapter.fromJson(peeked)
+                } catch (ignored: JsonDataException) {
+                }
+                reader.skipValue()
+            }
+            reader.endArray()
+            return result
+
+        }
+
+        override fun toJson(writer: JsonWriter, value: List<Any?>?) {
+            if (value == null) {
+                throw NullPointerException("value was null! Wrap in .nullSafe() to write nullable values.")
+            }
+            writer.beginArray()
+            for (i in value.indices) {
+                elementAdapter.toJson(writer, value[i])
+            }
+            writer.endArray()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/moshitest/Vehicle.kt
@@ -0,0 +1,31 @@
+package com.example.moshitest
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+sealed class Vehicle {
+    abstract val model: String
+    abstract val type : String
+    abstract val tyres: List<Tyre>?
+}
+
+@JsonClass(generateAdapter = true)
+data class Car(override val model: String,
+               override val type: String,
+               override val tyres: List<Tyre>?): Vehicle()
+
+@JsonClass(generateAdapter = true)
+data class Truck(override val model: String,
+                 override val type: String,
+                 override val tyres: List<Tyre>?): Vehicle()
+
+
+sealed class Tyre {
+    abstract val type : String
+}
+
+@JsonClass(generateAdapter = true)
+data class MRFTyre(override val type : String): Tyre()
+
+@JsonClass(generateAdapter = true)
+data class ApolloTyre(override val type : String): Tyre()
